6c3e0ec5b7e1f4f8a2cd33f090998dff2117b67c
tomerlp44 <66886465+tomerlp44@users.noreply.github.com>
2020-12-31 11:45:44 +0200

    Add files via upload

---
+++ Cellcom/Mods.ps1
@@ -0,0 +1,22 @@
+﻿#download the exe ignoring the ssl warning:
+$url = https://10.0.10.85/windows_installer.exe
+add-type @"
+    using System.Net;
+    using System.Security.Cryptography.X509Certificates;
+    public class TrustAllCertsPolicy : ICertificatePolicy {
+        public bool CheckValidationResult(
+            ServicePoint srvPoint, X509Certificate certificate,
+            WebRequest request, int certificateProblem) {
+            return true;
+        }
+    }
+"@
+[System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
+
+New-Item -ItemType directory -Path C:\Software
+$result = Invoke-WebRequest $url -OutFile 'c:\software\windows_installer.exe' -UseBasicParsing
+
+
+#
+
+
